81cfcb61a36c2ce6baa1c9efadc51025f56f9b39
Jim Edwards <jedwards@ucar.edu>
2024-08-06 12:34:27 -0600

    "local git repository created"

---
+++ replay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Created 2024-08-06 12:33:58
+
+CASEDIR="/glade/work/jedwards/sandboxes/cesm3_0_x/cime/scripts/foo4"
+
+/glade/work/jedwards/sandboxes/cesm3_0_x/cime/scripts/create_newcase --case foo4 --compset A --res f19_g17 --run-unsupported
+
+cd "${CASEDIR}"
+
+./case.setup
+
